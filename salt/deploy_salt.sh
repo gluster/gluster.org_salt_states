bc00e7ba37aa4010ab1c40eb395016d7edd3d09c
Michael Scherer <misc@zarb.org>
2015-03-05 13:14:49 +0100

    Do not erase /srv/salt with pillar by error

--- salt/deploy_salt.sh
+++ salt/deploy_salt.sh
@@ -3,7 +3,13 @@
 # TODO fix the path
 if  [[ $PWD == /srv/git_repos/* ]]; then
     # only update if run from git
-    GIT_WORK_TREE=/srv/salt git checkout -q -f
+    if [[ $PWD == /srv/git_repos/states* ]]; 
+        then export GIT_WORK_TREE=/srv/salt 
+    fi;
+    if [[ $PWD == /srv/git_repos/pillar* ]]; 
+        then export GIT_WORK_TREE=/srv/pillar 
+    fi;
+    git checkout -q -f
 fi
 salt \* state.highstate
 
